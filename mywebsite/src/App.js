da3cf2116229bc72b06e26fe1a5d9841c3201f7b
Salvador Ludovico <salvadorludovico.sl@gmail.com>
2021-01-12 18:07:56 -0300

    React Components

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,23 +1,55 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+// import "./App.css";
+
+class Students extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Name: {this.props.name}</h2>
+        <h3>Age: {this.props.age}</h3>
+        <AcademicData RA={this.props.ra} COURSE={this.props.studyArea} />
+        <hr></hr>
+      </div>
+    );
+  }
+}
+
+class AcademicData extends Component {
+  render() {
+    return (
+      <div>
+        <u>Academic Data</u>
+        <ul>
+          <li>RA: {this.props.RA}</li>
+          <li>Course: {this.props.COURSE}</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>House Students</h1>
+      <Students
+        name="Salvador"
+        age="20"
+        ra="769852"
+        studyArea="Computer Engineering"
+      />
+      <Students
+        name="Arthur"
+        age="22"
+        ra="769889"
+        studyArea="Mechatronics Engineering"
+      />
+      <Students
+        name="Caio"
+        age="25"
+        ra="769811"
+        studyArea="Civil Engineering"
+      />
     </div>
   );
 }
