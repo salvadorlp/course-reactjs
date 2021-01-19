3ec36549563eda0f6afade38f3197005707a55dc
Salvador Ludovico <salvadorludovico.sl@gmail.com>
2021-01-19 11:36:15 -0300

    StateLess and StateFull components

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,57 +1,74 @@
 import React, { Component } from "react";
-// import "./App.css";
 
-class Students extends Component {
+// StateLess x StateFull
+// in the majority of cases, Statefull components will be represented by classes
+//
+// import React, { Component } from "react";
+//
+// class App extends Component {
+//   render() {
+//     return(
+//     <h1>Olá ReactJS</h1>
+//     );
+//   }
+// }
+//
+// export default App;
+//
+// In this component you must use render() method because it is responsible for show and re-show informations on the screen
+//
+// The same component can be coded like this:
+// import React from "react";
+
+// const App = () => {
+//   return(
+//     <h1>Olá ReactJS</h1>
+//   );
+// };
+
+// export default App;
+
+class App extends Component {
+  constructor(props) {
+    super(props); // use all resources from this component
+    this.state = {
+      name: "Matheus",
+      count: 0,
+    };
+
+    this.increase = this.increase.bind(this); // connecting increase function to this class
+  }
+
+  increase() {
+    let state = this.state;
+    state.count += 1;
+
+    this.setState(state);
+  }
+
+  decrease() {
+    let state = this.state;
+    if (state.count === 0) {
+      alert("Can't reduce below zero!");
+      return;
+    }
+    state.count -= 1;
+
+    this.setState(state);
+  }
+
   render() {
     return (
       <div>
-        <h2>Name: {this.props.name}</h2>
-        <h3>Age: {this.props.age}</h3>
-        <AcademicData RA={this.props.ra} COURSE={this.props.studyArea} />
-        <hr></hr>
+        <h1>Counter</h1>
+        <h1>
+          <button onClick={this.decrease}>-</button>
+          {this.state.count}
+          <button onClick={this.increase}>+</button>
+        </h1>
       </div>
     );
   }
-}
-
-class AcademicData extends Component {
-  render() {
-    return (
-      <div>
-        <u>Academic Data</u>
-        <ul>
-          <li>RA: {this.props.RA}</li>
-          <li>Course: {this.props.COURSE}</li>
-        </ul>
-      </div>
-    );
-  }
-}
-
-function App() {
-  return (
-    <div>
-      <h1>House Students</h1>
-      <Students
-        name="Salvador"
-        age="20"
-        ra="769852"
-        studyArea="Computer Engineering"
-      />
-      <Students
-        name="Arthur"
-        age="22"
-        ra="769889"
-        studyArea="Mechatronics Engineering"
-      />
-      <Students
-        name="Caio"
-        age="25"
-        ra="769811"
-        studyArea="Civil Engineering"
-      />
-    </div>
-  );
 }
 
 export default App;
